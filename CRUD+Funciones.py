eaa1fded9808d5fde21dc8d63673ab2c3f4cc1a5
DanielV593 <juan.vasquez03@epn.edu.ec>
2025-07-15 12:11:14 -0500

    Update CRUD+Funciones.py

--- CRUD+Funciones.py
+++ CRUD+Funciones.py
@@ -1,1 +1,21 @@
-#CRUD
+import os
+archivo = "ususarios.txt"
+
+#1. Cargar los datos del archivo 
+
+def cargar_datos():
+    if not os.path.exists(archivo): 
+        return {}
+    with open(archivo, "r", encoding="utf-8") as file:
+        lineas = file.readlines()
+        datos = {}
+        for linea in lineas: 
+            try: 
+                usuario = eval(linea.strip())
+                correo = usuario["correo"]
+                datos[correo] = usuario
+            except: 
+                continue
+        return datos
+    
+#2. Apartado para guardar datos en el archivo creado... 
